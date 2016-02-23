69f4ff3b8f19a3f821c6d8bc352c7fc9bf58dbab
David Stolarsky <david.stolarsky@nytimes.com>
2016-02-23 18:13:41 -0500

    possibly legit formula

---
+++ Library/Formula/ansiblock.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class AnsiBlock < Formula
+  homepage 'https://github.com/gimlids/ansiblock'
+  head 'https://github.com/gimlids/ansiblock.git'
+  version "0.1"
+
+  depends_on "gist"
+
+  resource "ansi2html" do
+    url "https://pypi.python.org/packages/source/a/ansi2html/ansi2html-1.1.0.tar.gz"
+    sha256 "537fc4ececf872f83d5cbd218fb64bb49304d258487e283736bb9e5e64319ad3"
+  end
+
+  def install
+    resource("ansi2html").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
+    bin.install 'bin/ansiblock'
+  end
+end
