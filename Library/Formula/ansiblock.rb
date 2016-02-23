d33bf5f874f5dd4e1e958d1953a597dbda6fcc72
David Stolarsky <david.stolarsky@nytimes.com>
2016-02-23 17:12:53 -0500

    add 'gist' brew dep

--- Library/Formula/ansiblock.rb
+++ Library/Formula/ansiblock.rb
@@ -1,4 +1,7 @@
 class AnsiBlock < Formula
+
+  depends_on "gist"
+
   resource "ansi2html" do
     url "https://pypi.python.org/packages/source/a/ansi2html/ansi2html-1.1.0.tar.gz"
     sha256 "537fc4ececf872f83d5cbd218fb64bb49304d258487e283736bb9e5e64319ad3"
